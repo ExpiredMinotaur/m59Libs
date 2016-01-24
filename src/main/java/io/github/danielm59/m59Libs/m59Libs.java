9f6048d16a4318905920a26740098a8855750ccd
Daniel Mason <danielm59@gmail.com>
2016-01-24 15:37:40 +0000

    Basic Mod setup

---
+++ src/main/java/io/github/danielm59/m59Libs/m59Libs.java
@@ -0,0 +1,9 @@
+package io.github.danielm59.m59Libs;
+
+import io.github.danielm59.m59Libs.reference.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION)
+public class m59Libs
+{    
+}
---
+++ src/main/java/io/github/danielm59/m59Libs/reference/Reference.java
@@ -0,0 +1,13 @@
+package io.github.danielm59.m59Libs.reference;
+
+public class Reference
+{
+    
+    public static final String MODID      = "m59Libs";
+    public static final String MODNAME    = "m59 Libs";
+    public static final String VERSION    = "@VERSION@";
+    //public static final String CPROXY     = "io.github.danielm59.m59Libs.proxy.ClientProxy";
+    //public static final String SPROXY     = "io.github.danielm59.m59Libs.proxy.ServerProxy";
+    //public static final String GUIFACTORY = "io.github.danielm59.m59.libs.client.gui.GuiFactory";
+                                          
+}
